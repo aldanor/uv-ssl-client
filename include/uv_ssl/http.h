61efb5dca51be916a3908f38a78ab3486a778930
Ivan Smirnov <i.s.smirnov@gmail.com>
2017-06-19 00:01:13 +0200

    Build boilerplate for http support

---
+++ include/uv_ssl/http.h
@@ -0,0 +1,8 @@
+#ifndef UV_SSL_HTTP_H_
+#define UV_SSL_HTTP_H_
+
+namespace uv_ssl {
+
+}  // namespace uv_ssl
+
+#endif  // UV_SSL_HTTP_H_
---
+++ src/http.cpp
@@ -0,0 +1,8 @@
+#include <http_parser.h>
+
+#include <uv_ssl/http.h>
+
+namespace uv_ssl {
+
+}  // namespace uv_ssl
+
